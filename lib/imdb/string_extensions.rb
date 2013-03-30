33dfea5e7d792557d1b4a3ccc26c9552d0a0e860
Peter Schrammel <peter.schrammel@gmx.de>
2013-03-30 23:45:59 +0100

    string extensions are now ruby 2.0.0 compatible

--- lib/imdb/string_extensions.rb
+++ lib/imdb/string_extensions.rb
@@ -1,14 +1,18 @@
 require 'cgi'
-require 'iconv'
  
 module Imdb #:nordoc:
   module StringExtensions
-  
+    if  RUBY_VERSION.to_f < 1.9
     # Unescape HTML
+    require 'iconv'
     def imdb_unescape_html
       Iconv.conv("UTF-8", 'ISO-8859-1', CGI::unescapeHTML(self))
     end
-  
+    else
+    def imdb_unescape_html
+      encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace, :replace => '')
+    end
+  end
     # Strip tags
     def imdb_strip_tags
       gsub(/<\/?[^>]*>/, "")
